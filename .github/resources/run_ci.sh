e5e766c9f5e2e9694c8b72188bb49c4dfb16c4fb
Andy Sadler <ansadler@redhat.com>
2022-03-31 08:34:15 -0500

    Implement "it runs" CI checks
    As a part of the merge process, we should make sure that incoming
    changes don't break our ability to run tests.  To do this, setup a
    simple minikube cluster and attempt to run tests against an empty
    cluster.  If the setup resources of a test gets created (i.e. secret
    data), and if the service binding sees no change in its status field,
    consider it a successful run.
    
    Signed-off-by: Andy Sadler <ansadler@redhat.com>

---
+++ .github/resources/run_ci.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+unset -e
+
+./setup.sh
+./run_tests.sh
+if [ $? -ne 0 ]; then
+    if [ "$(kubectl get -n cts-namespace servicebindings.servicebinding.io -o jsonpath="{.items[].status}" | wc -m)" = "0" ] && [ "$(kubectl get -n cts-namespace secrets -o name | wc -l)" = "2" ]; then
+        exit 0
+    else
+        exit 1
+    fi
+fi
