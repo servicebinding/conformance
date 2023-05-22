8ee74b3a26113333983ec5043c071dbe6bf21e3c
Andy Sadler <ansadler@redhat.com>
2023-05-22 17:19:47 -0500

    Allow parallel acceptance tests
    Using behavex, we can have test scenarios run in parallel.  This can
    lead to a significant decrease in the amount of time it takes to run
    tests.  There are diminishing returns for the amount of runners we
    add, but overall, it's still faster.
    
    Signed-off-by: Andy Sadler <ansadler@redhat.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+usage() {
+    echo "$0 usage:"
+    grep " .)\ #" $0 | sed -e "s/^\s\+\([a-z]\).*)\ # \(.*\)$/\t-\1: \2/g"
+    exit 0
+}
+
+while getopts ":j:n:h" arg; do
+    case $arg in
+        j) # set number of jobs to use to run tests (may cause test instability!)
+            jobs=${OPTARG}
+            ;;
+        n) # set the namespace to run tests in
+            TEST_NAMESPACE=${OPTARG}
+            ;;
+        h) # display help
+            usage
+            exit 0
+            ;;
+    esac
+done
+
 OUTPUT_DIR=test-output
 if [ -f test-tmp-dir ]; then
     OUTPUT_DIR=$(cat test-tmp-dir)
@@ -10,13 +31,20 @@ PYTHON_VENV_DIR="${OUTPUT_DIR}/venv"
 
 mkdir -p "${OUTPUT_DIR}"
 
-TEST_NAMESPACE="servicebindings-cts"
+if [ -z $TEST_NAMESPACE ]; then
+    TEST_NAMESPACE="servicebindings-cts"
+fi
+
 TEST_ACCEPTANCE_OUTPUT_DIR="${OUTPUT_DIR}"/results
 
 if [ $(kubectl get namespace ${TEST_NAMESPACE} > /dev/null; echo $?) -eq '0' ]; then
     kubectl delete namespace ${TEST_NAMESPACE}
 fi
 
+if [ -z $jobs ]; then
+    jobs=1
+fi
+
 echo "Running acceptance tests"
 
-TEST_NAMESPACE=${TEST_NAMESPACE} "${PYTHON_VENV_DIR}/bin/behave" --junit --junit-directory ${TEST_ACCEPTANCE_OUTPUT_DIR} --no-capture --no-capture-stderr features
+FEATURES_PATH=features TEST_NAMESPACE=${TEST_NAMESPACE} "${PYTHON_VENV_DIR}/bin/behavex" -o ${TEST_ACCEPTANCE_OUTPUT_DIR} --capture --capture-stderr --parallel-processes ${jobs} --parallel-scheme scenario
